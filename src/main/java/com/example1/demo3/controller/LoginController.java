d2f59d2d2cdcd6e433e4efff1d7edbb1e6c8d065
tatu <sakanasakana121212@icloud.com>
2026-04-07 13:24:03 +0900

    ログイン画面・コントローラー実装、権限実装

---
+++ src/main/java/com/example1/demo3/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example1.demo3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+}
