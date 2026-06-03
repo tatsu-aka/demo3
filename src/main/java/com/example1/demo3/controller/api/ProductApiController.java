c6f0663e1687544a2c500d202929a9baba5d14af
tatu <sakanasakana121212@icloud.com>
2026-06-03 18:49:54 +0900

    不要なコードの削除

--- src/main/java/com/example1/demo3/controller/api/ProductApiController.java
+++ src/main/java/com/example1/demo3/controller/api/ProductApiController.java
@@ -2,26 +2,15 @@ package com.example1.demo3.controller.api;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example1.demo3.dto.ProductDto;
 import com.example1.demo3.dto.ProductMakerStockDto;
-import com.example1.demo3.dto.ProductRequest;
-import com.example1.demo3.repository.ProductRepository;
+
 import com.example1.demo3.repository.StockDetailRepository;
 import com.example1.demo3.service.ProductService;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/products")
@@ -32,47 +21,6 @@ public class ProductApiController {
     public ProductApiController(ProductService productService, StockDetailRepository stockDetailRepository) {
         this.productService = productService;
         this.stockDetailRepository = stockDetailRepository;
-    }
-
-    //商品一覧　（出力DTO）
-    @GetMapping
-    public List<ProductDto> getProducts() {
-        return productService.findAllDto();
-    }
-    
-    //商品登録　（入力DTO＋バリデーション）
-    @PostMapping
-    public ResponseEntity<?> createProduct(@Valid @RequestBody ProductRequest req, BindingResult result) {
-        if (result.hasErrors()) {
-            List<String> messages = result.getAllErrors().stream().map(ObjectError::getDefaultMessage).toList();
-            return ResponseEntity.badRequest().body(messages);
-        }
-
-        productService.create(req);
-        return ResponseEntity.ok("ok");
-    }
-
-    //商品更新
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateProduct(
-        @PathVariable Integer id,
-        @Valid @RequestBody ProductRequest req,
-        BindingResult result
-    ) {
-        if (result.hasErrors()) {
-            List<String> messages = result.getAllErrors().stream()
-            .map(ObjectError::getDefaultMessage).toList();
-            return ResponseEntity.badRequest().body(messages);
-        }
-        productService.update(id, req);
-        return ResponseEntity.ok("ok");
-    }
-
-    //商品削除
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
-        productService.deleteProduct(id);
-        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/list-by-maker")
